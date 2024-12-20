a8f7ddcc7fb4a62265196dfd1fef95f042e0532e
pedro.albuquerque <albpedro458@gmail.com>
2024-12-20 12:36:05 -0300

    add MedicationCard componente

---
+++ components/MedicationCard/index.tsx
@@ -0,0 +1,27 @@
+import { View } from "react-native";
+import { Card } from "react-native-paper";
+import { Image } from "expo-image";
+
+import styles from "./styles";
+
+type Props = {
+  medicationTitle: string;
+  chemicalComposition: string;
+};
+
+export default function MedicationCard({
+  chemicalComposition,
+  medicationTitle,
+}: Props) {
+  return (
+    <Card>
+      <Card.Title
+        title={medicationTitle}
+        subtitle={chemicalComposition}
+        right={(props) => (
+          <Image style={styles.image} source="https://picsum.photos/200" />
+        )}
+      />
+    </Card>
+  );
+}
---
+++ components/MedicationCard/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  image: {
+    width: 80,
+    height: 80,
+    borderTopRightRadius: 12,
+    borderBottomRightRadius: 12,
+  }
+})
