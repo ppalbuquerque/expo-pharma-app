78d4d1b70fca0171ff9e228fdd48fd827a5f8faa
pedro.albuquerque <albpedro458@gmail.com>
2024-12-27 09:51:14 -0300

    add fields to register screen

--- app/(medication)/register.tsx
+++ app/(medication)/register.tsx
@@ -1,9 +1,60 @@
-import { View, Text } from "react-native";
+import { View, ScrollView } from "react-native";
+
+import TextInput from "@/components/TextInput";
+
+import styles from "./styles";
 
 export default function Register() {
   return (
-    <View>
-      <Text>Registrar remédio</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.textInputContainer}>
+        <TextInput label="Nome" supportingText="Nome fantasia do remédio" />
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          label="Posologia"
+          supportingText="Como o remédio deve ser tomado"
+        />
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          label="Composição química"
+          supportingText="Nomes dos compostos químicos"
+        />
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          label="Posição na prateleira"
+          supportingText="Selecione qual é a posição do remédio na prateleira"
+        />
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          label="Preço da caixa"
+          supportingText="Preço do remédio vendido em caixa"
+          keyboardType="decimal-pad"
+        />
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          label="Preço da unidade"
+          supportingText="Preço de uma unidade do remédio"
+          keyboardType="decimal-pad"
+        />
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          label="Uso da medicação"
+          supportingText="Qual é o uso indicado dessa medicação"
+        />
+      </View>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          label="Estoque"
+          supportingText="Quantidade de unidades do remédio"
+          keyboardType="number-pad"
+        />
+      </View>
+    </ScrollView>
   );
 }
---
+++ app/(medication)/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    marginHorizontal: 16,
+    marginTop: 32
+  },
+  textInputContainer: {
+    marginBottom: 16
+  }
+})
