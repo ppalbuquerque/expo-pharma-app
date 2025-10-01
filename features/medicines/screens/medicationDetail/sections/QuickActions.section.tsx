58495a273d25d43264e0d362555eb3ffd0e3926e
ppalbuquerque <albpedro458@gmail.com>
2025-10-01 15:19:20 -0300

    implemented QuickActions section

---
+++ features/medicines/screens/medicationDetail/sections/QuickActions.section.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
+
+import PharmaCard from "@/shared/components/common/Card";
+import { OutlinedButton } from "@/shared/components/common/OutlinedButton";
+
+import styles from "./QuickActions.styles";
+
+export function QuickActionSections() {
+  return (
+    <PharmaCard style={styles.container}>
+      <Text style={styles.cardTitle}>Ações Rápidas</Text>
+      <OutlinedButton>
+        <Feather name="edit" size={24} color="black" style={styles.icon} />
+        <View>
+          <Text style={styles.buttonTitle}>Editar medicamento</Text>
+          <Text style={styles.buttonDescription}>
+            Atualizar as informações do medicamento
+          </Text>
+        </View>
+      </OutlinedButton>
+      <View style={styles.buttonContainer}>
+        <OutlinedButton>
+          <Feather
+            name="shopping-cart"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+          <View>
+            <Text style={styles.buttonTitle}>Atualizar estoque</Text>
+            <Text style={styles.buttonDescription}>
+              Modificar a sua quantidade em estoque
+            </Text>
+          </View>
+        </OutlinedButton>
+      </View>
+      <View style={styles.buttonContainer}>
+        <OutlinedButton type="danger">
+          <Feather name="trash-2" size={24} color="black" style={styles.icon} />
+          <View>
+            <Text style={styles.buttonTitle}>Apagar medicamento</Text>
+            <Text style={styles.buttonDescription}>
+              Apagar o registro do medicamento
+            </Text>
+          </View>
+        </OutlinedButton>
+      </View>
+    </PharmaCard>
+  );
+}
---
+++ features/medicines/screens/medicationDetail/sections/QuickActions.styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    marginTop: 24,
+  },
+  cardTitle: {
+    fontSize: 20,
+    lineHeight: 30,
+    marginBottom: 24,
+  },
+  buttonContainer: {
+    marginTop: 16,
+  },
+  buttonTitle: {
+    fontSize: 14,
+    lineHeight: 21,
+    color: "#222222",
+  },
+  buttonDescription: {
+    fontSize: 12,
+    lineHeight: 18,
+    color: "#717171",
+  },
+  icon: {
+    marginRight: 12,
+  },
+});
